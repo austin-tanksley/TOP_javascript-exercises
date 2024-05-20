b9958e32eda227df29f59cb9818bcb639fff4d58
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-20 16:42:42 -0500

    completed exercise 06-Leap Years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
+
+    if (year % 4 == 0){
+        isLeapYear = true;
+        if (year % 100 === 0 && year % 400 !== 0){
+            isLeapYear = false;
+        }
+    }
+    return isLeapYear;
 
 };
 
