132654b3a0c084c61518bbb3698ea3f2ffe146aa
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-20 10:36:36 -0500

    completed exercise 2 - repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    newStr='';
+    if (num>=0){
+        for (i = 0; i<num; i++) {
+            newStr = newStr.concat(string);
+        }
+        return newStr;
+    }
+    else {
+        return 'ERROR';
+    }
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
