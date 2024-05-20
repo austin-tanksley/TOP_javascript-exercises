55a2c09b1083caa4cefecbaf52fc38da1759144b
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-20 15:16:48 -0500

    working on test 5 of 6 on exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(in1, in2) {
+    sum = 0;
+    let floor = 0;
+    let ceiling = 0;
 
+    //returns error if inputs are negative
+    if (in1<0 || in2<0){
+        return 'ERROR'
+    }
+    else if (isNaN(in1)){
+        return 'ERROR';
+    }
+    else if (isNaN(in2)){
+        return 'ERROR';
+    }
+    if (in1>in2){
+        floor = in2;
+        ceiling = in1;
+    }
+    else {
+        floor = in1;
+        ceiling = in2;
+    }
+    for (i = floor; i < (ceiling +1); i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
