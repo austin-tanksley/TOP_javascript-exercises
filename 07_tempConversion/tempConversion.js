df4854168acdb371b681978de631606c1b72963a
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-20 21:09:26 -0500

    completed exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,13 @@
 const convertToCelsius = function(tempF) {
-  let tempC = (tempF) => {
-    (tempF-32) * (5/9);
-  }
+  let tempC = (tempF-32) * (5/9);
   let rounded = Math.round(10*tempC)/10;
   return rounded;
 };
 
 const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * (9/5)) + 32
+  let rounded = Math.round(10*tempF)/10;
+  return rounded;
 };
 
 // Do not edit below this line
