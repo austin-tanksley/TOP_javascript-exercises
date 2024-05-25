9645758f73daa9cd541bdf524fa18956dc8b2e17
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-25 01:10:51 -0500

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (inStr) {
+    const acceptable = '0123456789abcdefghijklmnopqrstuvwxyz';
+    const stripped = inStr
+        .toLowerCase()
+        .split("")
+        .filter((x)=> acceptable.includes(x))
+        .join("");
+    const reversed = stripped.split('').reverse().join('');
 
+    return (reversed === stripped) ? true : false;
 };
 
 // Do not edit below this line
