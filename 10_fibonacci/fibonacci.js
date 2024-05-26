1e8d06637a076d32ea385399c51aafd915a3a298
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-26 13:00:37 -0500

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    fib = [];
+    if (num<0){
+        return 'OOPS'
+    }
+    if (typeof(num)=="string"){
+        num = Number(num);
+    }
+    if (num === 0 || typeof(num)!= "number"){
+        return 0;
+    }
+    while ( fib.length < 2) {
+        fib.push(1);
+    }
+    for (i = 2; i < num; i++) {
+        fib.push((fib[i-1])+(fib[i-2]));
+    };
+    return fib[fib.length-1];
+    // return fib;
 };
 
 // Do not edit below this line
